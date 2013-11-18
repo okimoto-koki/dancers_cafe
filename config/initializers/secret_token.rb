67dc4ad774a4e796fe3a0d6e0609384f99da8c42
kota777 <kota777jp@gmail.com>
2013-11-19 00:20:08 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DancersCafe::Application.config.secret_key_base = 'b33c946b75fd854f2149a570bc2fc382bbd89d6534b0d580b8d4cc5d78073720b2893ef0bb0ecb9a567609e68202c191ee23c0a6a36c5d1805290dd14287adc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DancersCafe::Application.config.session_store :cookie_store, key: '_dancers_cafe_session'
